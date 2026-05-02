1e160ed0dd1f8fadedd197288dc64d8ba0a3c5e0
Micah King <micahndungu01@gmail.com>
2026-05-02 11:16:47 +0000

    Build: Fix paths and compile production assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/MR_Cosmetics_Web_Store-5.1.1/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
